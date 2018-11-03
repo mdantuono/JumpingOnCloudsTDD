9c87db48b2c162aabb1d1c675ba4abc16b219ff0
Michael <mikeishere3@gmail.com>
2018-11-03 13:52:39 -0400

    created 4 tests, all passing

---
+++ JumpingOnClouds.UnitTest/CloudJumps.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpingOnClouds.UnitTest
+{
+    class CloudJumps
+    {
+        internal int FindNumberOfJumps(int[] cloudArray)
+        {
+            // Initialize jump counter
+            int numberOfJumps = 0;
+
+            for (var i = 0; i < cloudArray.Length - 1; i++)
+            {
+                if (cloudArray[i + 1] == 0)
+                {
+                    if (i + 1 < cloudArray.Length - 1 && cloudArray[i + 2] == 0)
+                    {
+                        numberOfJumps++;
+                        i++;
+                    }
+                    else
+                    {
+                        numberOfJumps++;
+                    }
+                } else
+                {
+                    if (cloudArray[i + 2] == 0)
+                    {
+                        numberOfJumps++;
+                        i++;
+                    }
+                }
+            }
+
+            return numberOfJumps;
+        }
+    }
+}
---
+++ JumpingOnClouds.UnitTest/CloudTest.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JumpingOnClouds.UnitTest
+{
+    public class CloudTest
+    {
+        [Test]
+        public void returnsAnInteger()
+        {
+            int[] cloudArray = new int[] { };
+
+            var cloudJumps = new CloudJumps();
+
+            int ourInteger = cloudJumps.FindNumberOfJumps(cloudArray);
+
+            bool ourIntegerIsAnInteger = (ourInteger.GetType() == typeof(int));
+
+            bool numberIsAnInteger = true;
+
+            Assert.AreEqual(ourIntegerIsAnInteger, numberIsAnInteger);
+
+        }
+
+        [Test]
+        public void FindNumberOfJumpsShouldReturnFour()
+        {
+            const int numberOfClouds = 7;
+            int[] cloudArray = new int[numberOfClouds] { 0, 0, 1, 0, 0, 1, 0 };
+            var expectedResult = 4;
+
+            var cloudJumps = new CloudJumps();
+
+            int result = cloudJumps.FindNumberOfJumps(cloudArray);
+
+            Assert.AreEqual(expectedResult, result);
+
+        }
+
+        [Test]
+        public void FindNumberOfJumpsShouldReturnThree()
+        {
+            const int numberOfClouds = 6;
+            int[] cloudArray = new int[numberOfClouds] { 0, 0, 0, 0, 1, 0 };
+            var expectedResult = 3;
+
+            var cloudJumps = new CloudJumps();
+
+            int result = cloudJumps.FindNumberOfJumps(cloudArray);
+
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void FindNumberOfJumpsShouldReturnThreeAgain()
+        {
+            const int numberOfClouds = 6;
+            int[] cloudArray = new int[numberOfClouds] { 0, 0, 0, 1, 0, 0 };
+            var expectedResult = 3;
+
+            var cloudJumps = new CloudJumps();
+
+            int result = cloudJumps.FindNumberOfJumps(cloudArray);
+
+            Assert.AreEqual(expectedResult, result);
+
+        }
+
+    }
+}
